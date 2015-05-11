5fe49060d37c3903ba33932191b845cec5f9add9
nguyenlehuy <huy.nguyenle@smartdev.vn>
2015-05-11 11:31:38 +0700

    add port listen

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
+app.set('port', (process.env.PORT || 5000));
 app.get("/home",function(re,res){
   res.end("mot con vit");
 });
 app.get("/",function(re,res){
   res.end("hai con heo");
 });
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port:" + app.get('port'))
+})
