4556c566ebb746fe3f313c33a54089b00e0503b6
nguyenlehuy <huy.nguyenle@smartdev.vn>
2015-05-11 09:54:03 +0700

    add package.json

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var app = express();
 app.get("/home",function(re,res){
   res.end("mot con vit");
 });
-app.listen(8000);
+app.get("/",function(re,res){
+  res.end("hai con heo");
+});
+app.listen(3000);
